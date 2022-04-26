5334d74039f1a3f945d4853ae5e296b07e29cc84
punithbg <50261733+punithbg@users.noreply.github.com>
2022-04-26 10:30:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   const [favourite,setFavourite]=useState([])
 
   const getMovies =async (searchValue)=>{
-      const url=`http://www.omdbapi.com/?s=${searchValue}&apikey=4e077ff7`
+      const url=`https://www.omdbapi.com/?s=${searchValue}&apikey=4e077ff7`
       const response=await fetch(url)
       const responseJson= await response.json()
       if(responseJson.Search){
